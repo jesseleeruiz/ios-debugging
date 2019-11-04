bddcefc8bba41d9287289607c8bc471148c1c050
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-11-04 10:01:07 -1000

    Initial Commit. Added own firebase to the project.

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,8 +9,7 @@
 import Foundation
 import CoreData
 
-#error("Change this value to your own firebase database! (and then delete this line)")
-let baseURL = URL(string: "https://journal-syncing.firebaseio.com/")!
+let baseURL = URL(string: "https://journal-a73f0.firebaseio.com/")!
 
 class EntryController {
     
